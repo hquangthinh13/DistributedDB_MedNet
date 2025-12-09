71f64ed2b282a45d075e2175c9047c1389562d36
Đan Thanh <thanhtrn754@gmail.com>
2025-12-09 19:45:21 +0700

    0.2
    Setup update

--- Branch2_Clinic/01_SetUpConnection.sql
+++ Branch2_Clinic/01_SetUpConnection.sql
@@ -1,38 +1,37 @@
-CREATE LOGIN Branch_3 WITH PASSWORD = 'thinh1234';
+CREATE LOGIN Branch_2 WITH PASSWORD = 'thanh1234';
 
-USE Branch_3;
+USE Branch_2;
 GO
 
-CREATE USER Branch_3 FOR LOGIN Branch_3;
-ALTER ROLE db_owner ADD MEMBER Branch_3;
+CREATE USER Branch_2 FOR LOGIN Branch_2;
+ALTER ROLE db_owner ADD MEMBER Branch_2;
 
---Linked servers:
-----Branch 1:
+SELECT name, type_desc
+FROM sys.server_principals
+WHERE name = 'Branch_2';
+
+--Connect Branch 1----
 EXEC sp_addlinkedserver   
-    @server     = 'Branch_1',     
-    @srvproduct = '',                  
-    @provider   = 'SQLOLEDB',            
-    @datasrc    = '26.50.54.179';    
-
+    @server     = 'Branch_1',      -- name you assign
+    @srvproduct = '',                    -- can be empty for SQL Server
+    @provider   = 'SQLOLEDB',             -- or 'MSOLEDBSQL', 'SQLOLEDB'
+    @datasrc    = '26.50.54.179';    -- network name or IP
 EXEC sp_addlinkedsrvlogin   
     @rmtsrvname = 'Branch_1',
     @useself    = 'False',
-    @locallogin = NULL,                 
+    @locallogin = NULL,                  -- all local logins
     @rmtuser    = 'Branch_1',
     @rmtpassword= 'ngoc1234';
 
-----Branch 2:
+---Connect Branch 3---
 EXEC sp_addlinkedserver   
-    @server     = 'Branch_2',     
-    @srvproduct = '',                  
-    @provider   = 'SQLOLEDB',            
-    @datasrc    = '26.111.241.214';    
-
+    @server     = 'Branch_3',      -- name you assign
+    @srvproduct = '',                    -- can be empty for SQL Server
+    @provider   = 'SQLOLEDB',             -- or 'MSOLEDBSQL', 'SQLOLEDB'
+    @datasrc    = '26.118.79.75';    -- network name or IP
 EXEC sp_addlinkedsrvlogin   
-    @rmtsrvname = 'Branch_2',
+    @rmtsrvname = 'Branch_3',
     @useself    = 'False',
-    @locallogin = NULL,                 
-    @rmtuser    = 'Branch_2',
-    @rmtpassword= 'thanh1234';
-
-
+    @locallogin = NULL,                  -- all local logins
+    @rmtuser    = 'Branch_3',
+    @rmtpassword= 'thinh1234';
